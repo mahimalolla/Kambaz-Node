593840a90cd21c23c71f78b7cc0d105ac12b3f6e
Mahima Lolla <74255400+mahimalolla@users.noreply.github.com>
2025-06-17 16:01:27 -0400

    Update modules.js

--- Kambaz/Database/modules.js
+++ Kambaz/Database/modules.js
@@ -1,1 +1,68 @@
-
+export default [
+  {
+    _id: "M101",
+    name: "Introduction to React",
+    description: "Basic React concepts and JSX",
+    course: "RS101"
+  },
+  {
+    _id: "M102",
+    name: "React State and Props",
+    description: "Managing component state and props",
+    course: "RS101"
+  },
+  {
+    _id: "M103",
+    name: "React Hooks",
+    description: "Using React hooks for state management",
+    course: "RS101"
+  },
+  {
+    _id: "M201",
+    name: "Node.js Fundamentals",
+    description: "Introduction to Node.js and server-side JavaScript",
+    course: "RS102"
+  },
+  {
+    _id: "M202",
+    name: "Express.js Framework",
+    description: "Building web applications with Express.js",
+    course: "RS102"
+  },
+  {
+    _id: "M301",
+    name: "Database Design Principles",
+    description: "Fundamentals of relational database design",
+    course: "RS103"
+  },
+  {
+    _id: "M302",
+    name: "SQL Programming",
+    description: "Writing SQL queries and database operations",
+    course: "RS103"
+  },
+  {
+    _id: "M401",
+    name: "Programming Fundamentals",
+    description: "Basic programming concepts and problem solving",
+    course: "RS104"
+  },
+  {
+    _id: "M501",
+    name: "Data Structures",
+    description: "Arrays, linked lists, stacks, and queues",
+    course: "RS105"
+  },
+  {
+    _id: "M502",
+    name: "Algorithm Analysis",
+    description: "Big O notation and algorithm complexity",
+    course: "RS105"
+  },
+  {
+    _id: "M601",
+    name: "Machine Learning Basics",
+    description: "Introduction to ML algorithms and concepts",
+    course: "RS106"
+  }
+];
