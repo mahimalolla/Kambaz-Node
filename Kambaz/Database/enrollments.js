5b8d5aaabe34930093f923661b660dd3b6178f5c
Mahima Lolla <74255400+mahimalolla@users.noreply.github.com>
2025-06-17 16:01:15 -0400

    Update enrollments.js

--- Kambaz/Database/enrollments.js
+++ Kambaz/Database/enrollments.js
@@ -1,1 +1,13 @@
-
+export default [
+  { _id: "1", user: "122", course: "RS101" }, // Bob enrolled in Web Development
+  { _id: "2", user: "122", course: "RS102" }, // Bob enrolled in Software Engineering
+  { _id: "3", user: "124", course: "RS101" }, // Diana enrolled in Web Development
+  { _id: "4", user: "124", course: "RS103" }, // Diana enrolled in Database Design
+  { _id: "5", user: "121", course: "RS101" }, // Alice teaches Web Development
+  { _id: "6", user: "121", course: "RS102" }, // Alice teaches Software Engineering
+  { _id: "7", user: "121", course: "RS103" }, // Alice teaches Database Design
+  { _id: "8", user: "123", course: "RS101" }, // Charlie teaches Web Development
+  { _id: "9", user: "123", course: "RS102" }, // Charlie teaches Software Engineering
+  { _id: "10", user: "125", course: "RS101" }, // Iron Man enrolled in Web Development
+  { _id: "11", user: "125", course: "RS106" }, // Iron Man enrolled in Machine Learning
+];
