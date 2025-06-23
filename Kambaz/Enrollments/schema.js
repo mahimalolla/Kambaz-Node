165b0a0148d4613252e8abeb0812b1562b377527
Mahima Lolla <74255400+mahimalolla@users.noreply.github.com>
2025-06-23 19:17:46 -0400

    Update schema.js

--- Kambaz/Enrollments/schema.js
+++ Kambaz/Enrollments/schema.js
@@ -1,1 +1,17 @@
+import mongoose from "mongoose";
 
+const enrollmentSchema = new mongoose.Schema({
+  _id: String,
+  course: { type: String, ref: "CourseModel" },
+  user: { type: String, ref: "UserModel" },
+  grade: Number,
+  letterGrade: String,
+  enrollmentDate: { type: Date, default: Date.now },
+  status: {
+    type: String,
+    enum: ["ENROLLED", "DROPPED", "COMPLETED"],
+    default: "ENROLLED",
+  },
+}, { collection: "enrollments" });
+
+export default enrollmentSchema;
