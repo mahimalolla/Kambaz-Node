b3e23bdb06aaf093721d94f7fa8acf7a653e35c5
Mahima Lolla <74255400+mahimalolla@users.noreply.github.com>
2025-06-23 19:14:14 -0400

    Update schema.js

--- Kambaz/Courses/schema.js
+++ Kambaz/Courses/schema.js
@@ -1,1 +1,17 @@
+import mongoose from "mongoose";
 
+const courseSchema = new mongoose.Schema({
+  _id: String,
+  name: String,
+  number: String,
+  credits: Number,
+  description: String,
+  startDate: Date,
+  endDate: Date,
+  image: String,
+  semester: String,
+  year: Number,
+  instructor: String,
+}, { collection: "courses" });
+
+export default courseSchema;
