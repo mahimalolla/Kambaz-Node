4d6ce3f18c98f1633332c801119cff1cae7de08d
Mahima Lolla <74255400+mahimalolla@users.noreply.github.com>
2025-06-23 19:14:39 -0400

    Update dao.js

--- Kambaz/Courses/dao.js
+++ Kambaz/Courses/dao.js
@@ -1,50 +1,30 @@
-import Database from "../Database/index.js";
+import model from "./model.js";
 import { v4 as uuidv4 } from "uuid";
 
 export function findAllCourses() {
-  return Database.courses;
+  return model.find();
 }
 
 export function findCourseById(courseId) {
-  return Database.courses.find((course) => course._id === courseId);
+  return model.findById(courseId);
 }
 
 export function createCourse(course) {
   const newCourse = { ...course, _id: uuidv4() };
-  Database.courses = [...Database.courses, newCourse];
-  return newCourse;
+  return model.create(newCourse);
 }
 
 export function updateCourse(courseId, courseUpdates) {
-  const { courses } = Database;
-  const courseIndex = courses.findIndex((course) => course._id === courseId);
-  if (courseIndex !== -1) {
-    const updatedCourse = { ...courses[courseIndex], ...courseUpdates };
-    Database.courses[courseIndex] = updatedCourse;
-    return updatedCourse;
-  }
-  return null;
+  return model.updateOne({ _id: courseId }, { $set: courseUpdates });
 }
 
 export function deleteCourse(courseId) {
-  const { courses, enrollments } = Database;
-  const courseIndex = courses.findIndex((course) => course._id === courseId);
-  if (courseIndex !== -1) {
-    // Remove the course
-    Database.courses = courses.filter((course) => course._id !== courseId);
-    // Remove all enrollments for this course
-    Database.enrollments = enrollments.filter(
-      (enrollment) => enrollment.course !== courseId
-    );
-    return true;
-  }
-  return false;
+  return model.deleteOne({ _id: courseId });
 }
 
+// This function will be moved to enrollments DAO later
 export function findCoursesForEnrolledUser(userId) {
-  const { courses, enrollments } = Database;
-  const enrolledCourses = courses.filter((course) =>
-    enrollments.some((enrollment) => enrollment.user === userId && enrollment.course === course._id)
-  );
-  return enrolledCourses;
+  // This will be handled by enrollments DAO
+  // For now, return empty array - we'll implement this with enrollments
+  return [];
 }
