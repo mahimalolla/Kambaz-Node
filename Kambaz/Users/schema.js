76a8f6ffc92fb2c07f325711c59e08fadc6f551d
Mahima Lolla <74255400+mahimalolla@users.noreply.github.com>
2025-06-23 18:50:31 -0400

    Update schema.js

--- Kambaz/Users/schema.js
+++ Kambaz/Users/schema.js
@@ -5,8 +5,8 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   firstName: String,
-  email: String,
   lastName: String,
+  email: String,
   dob: Date,
   role: {
     type: String,
