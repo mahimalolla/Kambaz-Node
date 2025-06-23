80c594bfefd89efee9afbe0bef83c3293fdc059d
Mahima Lolla <74255400+mahimalolla@users.noreply.github.com>
2025-06-23 19:20:40 -0400

    Update schema.js

--- Kambaz/Modules/schema.js
+++ Kambaz/Modules/schema.js
@@ -1,1 +1,12 @@
+import mongoose from "mongoose";
 
+const moduleSchema = new mongoose.Schema({
+  _id: String,
+  name: String,
+  description: String,
+  course: { type: String, ref: "CourseModel" },
+  lessons: [String], // Array of lesson IDs if you have lessons
+  editing: { type: Boolean, default: false },
+}, { collection: "modules" });
+
+export default moduleSchema;
